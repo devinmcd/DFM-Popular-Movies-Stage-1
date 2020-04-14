776e2a2a0f29073deb6b785ec57eecb884c77604
i863560 <i863560@Devins-MacBook-Pro.local>
2020-04-14 01:04:49 -0700

    Set up network connection

--- app/src/main/java/com/example/dfmpopularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/dfmpopularmoviesstage1/MainActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.example.dfmpopularmoviesstage1.Utils.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +27,31 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler_view);
         GridLayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 2);
         recyclerView.setLayoutManager(layoutManager);
+
+        text = findViewById(R.id.test);
+
+        new MovieDataTask().execute("popular");
+    }
+
+    private class MovieDataTask extends AsyncTask<String, Void, String> {
+        String t;
+
+        @Override
+        protected String doInBackground(String... params) {
+            URL url = NetworkUtils.buildUrl(params[0]);
+            try {
+                t = NetworkUtils.getResponseFromHttpUrl(url);
+                return t;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            text.setText(t);
+        }
     }
 }
