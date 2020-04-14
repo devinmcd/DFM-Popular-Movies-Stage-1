3a360e2537144d2c551a0f7862f1ec37f70fa0a5
i863560 <i863560@Devins-MacBook-Pro.local>
2020-04-14 00:04:59 -0700

    test

---
+++ app/src/main/java/com/example/dfmpopularmoviesstage1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.dfmpopularmoviesstage1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+}
